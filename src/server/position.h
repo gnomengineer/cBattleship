1fa88a682551b05c296da6ee8bf3aa4972267e46
Mogria <m0gr14@gmail.com>
2014-09-19 13:31:28 +0200

    implemented simple position type as std::pair

---
+++ src/server/position.h
@@ -0,0 +1,8 @@
+#ifndef _POSITION_H
+#define _POSITION_H
+
+#include <utility>
+typedef unsigned int position_coordinate_t;
+typedef std::pair<position_coordinate_t, position_coordinate_t> position_t;
+
+#endif
