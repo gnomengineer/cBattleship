b4cc34755d2f216dfdeae5276d055b3eec6f0a85
Mogria <m0gr14@gmail.com>
2014-09-19 13:31:56 +0200

    implemented Field class

---
+++ src/server/Field.cpp
@@ -0,0 +1,17 @@
+#include "Field.h"
+
+Field::Field(position_t position)
+    : position(position), hit(false) {
+}
+
+position_t Field::get_position() {
+    return position;
+}
+
+bool Field::is_hit() {
+    return hit;
+}
+
+bool Field::set_hit() {
+    hit = true;
+}
---
+++ src/server/Field.h
@@ -0,0 +1,20 @@
+#ifndef _FIELD_H
+#define _FIELD_H
+
+#include "position.h"
+
+class Field {
+    private:
+        position_t position;
+        bool hit;
+
+    public:
+        Field(position_t position);
+
+        position_t get_position();
+        bool is_hit();
+        bool set_hit();
+
+};
+#endif
+
