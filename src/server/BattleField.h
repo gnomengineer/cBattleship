98978dc93aa7f086c957d3967184464d57f78344
Troopa <troopa.der.pate@gmail.com>
2014-09-19 13:06:11 +0200

    first classes to try cmake

---
+++ src/server/BattleField.h
@@ -0,0 +1,12 @@
+/*************************************************************
+*
+* filename: BattleField.h 
+*
+* @author: troopa 
+*
+* date: 2014-09-19 
+*
+* description: This class provides the functionality of a battle
+* field. It gives methods for reading position of ships
+*
+*************************************************************/
