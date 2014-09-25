eee911f6f32359c084bc803a92b0a3e57fe09511
Mogria <m0gr14@gmail.com>
2014-09-25 07:26:17 +0200

    added cppunit to project

---
+++ src/test/main.cpp
@@ -0,0 +1,13 @@
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+int main( int argc, char **argv)
+{
+    CppUnit::TextUi::TestRunner runner;
+    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+    runner.addTest(registry.makeTest());
+    runner.setOutputter(new CppUnit::CompilerOutputter(&runner.result(), std::cerr));
+    bool wasSuccessful = runner.run("", false);
+    return !wasSuccessful;
+}
