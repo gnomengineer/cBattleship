230be29a01f137e07c131db0ddb5ea35c6846cb7
Troopa <troopa.der.pate@gmail.com>
2014-11-12 08:33:03 +0100

    uploaded UI classes (not tested nor implemented yet)

--- src/client/BattleFieldUI.cpp
+++ src/client/BattleFieldUI.cpp
@@ -1,6 +1,12 @@
 #include "BattleFieldUI.h"
 
 BattleFieldUI::BattleFieldUI(){
+    initscr();
+    curs_set(1);
+    
+
+    home_win = subwin(stdscr,BattleField.BATTLEFIELD_HEIGHT,BattleField.BATTLEFIELD_WIDTH, 10, 20);
+    enemy_win = subwin(stdscr,BattleField.BATTLEFIELD_HEIGHT,BattleField.BATTLEFIELD_WIDTH, 10, BattleField.BATTLEFIELD_WIDTH + 30);
 
 }
 
@@ -9,7 +15,7 @@ void BattleFieldUI::hide_field(WINDOW *win){
 }
 
 void BattleFieldUI::draw_field(){
-
+    
 }
 
 void BattleFieldUI::draw_ship(int length, int orientation, pos_t position){
