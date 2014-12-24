2433f310520a04410445665185c915e4cf8c9026
Troopa <troopa.der.pate@gmail.com>
2014-12-24 18:24:18 +0100

    removed compiler error

--- src/client/BattleFieldUI.h
+++ src/client/BattleFieldUI.h
@@ -5,19 +5,25 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+typedef enum{
+    SHIP = 0,
+    EMPTY = 1,
+    SHIP_Hit = 2,
+    MISS = 3
+} graphic_content;
 
 class BattleFieldUI {
     private:
 WINDOW *home_win;
-char[][] home_content;
+char home_content[BATTLEFIELD_HEIGHT][BATTLEFIELD_WIDTH] ;
 WINDOW *enemy_win;
-char[][] enemy_content;
+char enemy_content[BATTLEFIELD_HEIGHT][BATTLEFIELD_WIDTH] ;
     public:
 BattleFieldUI();
 void hide_field(WINDOW *win);
 void draw_field();
-void draw_ship(int length, int orientation, pos_t position);
-void draw_hit_mark(WINDOW *win, pos_t position, bool is_ship);
+void draw_ship(int length, int orientation, position_t position);
+void draw_hit_mark(WINDOW *win, position_t position, bool is_ship);
 void show_field(WINDOW *win);
-}
+};
 #endif
