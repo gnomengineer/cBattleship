b0ff698301c47f6db2bcb7f987df727e7062bdd4
Mogria <m0gr14@gmail.com>
2015-03-05 22:34:07 +0100

    fixed scripts/generate-network-package to generate *Package.h files instead of *Command.h files fixed scripts/generate-network-package to generate Package classes instead of Command classes

--- src/common/communication/GameReadyPackage.h
+++ src/common/communication/GameReadyPackage.h
@@ -1,14 +1,21 @@
 #ifndef _GAMEREADYPACKAGE_H
 #define _GAMEREADYPACKAGE_H
 
-#include <string>
 #include <vector>
 
 #include "NetworkPackage.h"
 class GameReadyPackage : public NetworkPackage {
+    private:
+        int field;
+
+
     public:
         virtual package_nr_t get_package_nr();
         virtual std::vector<unsigned char> encode_payload();
         virtual void decode_payload(std::vector<unsigned char> command_data);
+
+        int get_field();
+        void set_field(int field);
+
 };
-#endif
+#endif /* _GAMEREADYPACKAGE_H */
