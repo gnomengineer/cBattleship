f2c4dfa57efd5eb8ef63a04cca1bf2fa8758f29d
Mogria <m0gr14@gmail.com>
2015-03-10 22:46:29 +0100

    added AuthenticatedNetworkPackage for all Packages which have an identity Currently only PlayerJoinAnswerPackage uses it to send the identity to the player other packages from the player should then be derived from AuthenticatedNetworkPackage in order to send the identity to the Server so it can be validated

---
+++ src/common/communication/AuthenticatedNetworkPackage.h
@@ -0,0 +1,23 @@
+#ifndef _AUTHENTICATEDNETWORKPACKAGE_H
+#define _AUTHENTICATEDNETWORKPACKAGE_H
+
+#include <vector>
+#include <string>
+
+#include "NetworkPackage.h"
+class AuthenticatedNetworkPackage : public NetworkPackage {
+    private:
+        std::string identity;
+
+    public:
+        AuthenticatedNetworkPackage();
+        virtual ~AuthenticatedNetworkPackage();
+
+        virtual package_nr_t get_package_nr() = 0;
+        virtual std::vector<unsigned char> encode_payload();
+        virtual void decode_payload(std::vector<unsigned char> command_data);
+
+        std::string get_identity();
+        void set_identity(std::string identity);
+};
+#endif /* _AUTHENTICATEDNETWORKPACKAGE_H */
--- src/common/communication/PlayerJoinAnswerPackage.h
+++ src/common/communication/PlayerJoinAnswerPackage.h
@@ -4,20 +4,12 @@
 #include <string>
 #include <vector>
 
-#include "NetworkPackage.h"
-class PlayerJoinAnswerPackage : public NetworkPackage {
-    private:
-        std::string identity;
-
+#include "AuthenticatedNetworkPackage.h"
+class PlayerJoinAnswerPackage : public AuthenticatedNetworkPackage {
     public:
         PlayerJoinAnswerPackage();
         virtual ~PlayerJoinAnswerPackage();
 
         virtual package_nr_t get_package_nr();
-        virtual std::vector<unsigned char> encode_payload();
-        virtual void decode_payload(std::vector<unsigned char> command_data);
-
-        std::string get_identity();
-        void set_identity(std::string identity);
 };
 #endif 
