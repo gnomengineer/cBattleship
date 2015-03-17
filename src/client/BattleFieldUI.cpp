7e31301b5d23c452743403ffbae7747d16eafaac
Troopa <troopa.der.pate@gmail.com>
2015-03-17 15:56:27 +0100

    implemented BattleFieldUI (work in progress)

--- src/client/BattleFieldUI.cpp
+++ src/client/BattleFieldUI.cpp
@@ -2,31 +2,56 @@
 
 BattleFieldUI::BattleFieldUI(){
     initscr();
+    start_color();
     curs_set(1);
     
+    //define normal COLOR_PAIR
+    init_pair(1, COLOR_BLUE,COLOR_BLACK);
+    init_pair(2, COLOR_BLACK, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+    init_parr(4, COLOR_GREEN, COLOR_Black);
+
+    home_content
 
     home_win = subwin(stdscr,BATTLEFIELD_HEIGHT,BATTLEFIELD_WIDTH, 10, 20);
     enemy_win = subwin(stdscr,BATTLEFIELD_HEIGHT,BATTLEFIELD_WIDTH, 10, BATTLEFIELD_WIDTH + 30);
 
 }
 
-void BattleFieldUI::hide_field(WINDOW *win){
-
+void BattleFieldUI::hide_field(){
+    wcolor_set(home_win, COLOR_PAIR(2));
+    draw_field(home_win,home_content);
+    wcolor_set(home_win, COLOR_PAIR(1));
 }
 
-void BattleFieldUI::draw_field(){
-    std::vector<unsigned char> home_char = home_content.to_vector(false);
+void BattleFieldUI::draw_field(WINDOW *win, BattleField field){
+    std::vector<std::vector<unsigned char>> field_vector = field.to_vector(false);
     int y = 0;
-    for(int i = 0; i < home_char.length(); i++){
-        if(i % BATTLEFIELD_WIDTH == 0)
-            y++;
+    for(auto field_vector_itr = field_vector.begin(); field_vector_itr != field_vector.end(); field_vector_itr++){
+        y++;
+        int x = 0;
+        for(auto itr = field_vector_itr.begin(); itr != field_vector_itr.end(); itr++){
+            mvwaddch(win,y,x++,itr);
+        }
     }   
 }
 
 void BattleFieldUI::draw_hit_mark(WINDOW *win, position_t position, bool is_ship){
-
+    if(is_ship){
+        mvwaddch(win,position.y,position.x,'#');
+    } else {
+        mvwaddch(win,position.y,position.x,'X');
+    }
 }
 
 void BattleFieldUI::show_field(WINDOW *win){
+    
+}
 
+WINDOW get_home_win(){
+    return home_win;
+}
+
+WINDOW get_enemy_win(){
+    return enemy_win;
 }
--- src/client/BattleFieldUI.h
+++ src/client/BattleFieldUI.h
@@ -11,11 +11,16 @@ WINDOW *home_win;
 BattleField home_content;
 WINDOW *enemy_win;
 BattleField enemy_content;
+bool is_home_shown = false;
+bool is_enemy_shown = false;
     public:
 BattleFieldUI();
-void hide_field(WINDOW *win);
-void draw_field();
+void hide_field();
+void draw_field(WINDOW *win);
 void draw_hit_mark(WINDOW *win, position_t position, bool is_ship);
 void show_field(WINDOW *win);
+//getter method
+WINDOW get_home_win();
+WINDOW get_enemy_win();
 };
 #endif
