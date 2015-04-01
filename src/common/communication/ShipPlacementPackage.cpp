e7685c4b3d14dbbe78f69af927be96121b722f14
Mogria <m0gr14@gmail.com>
2015-04-01 22:55:08 +0200

    ShipPlacementPackage now uses new utitlity methods form NetworkPackage

--- src/common/communication/ShipPlacementPackage.cpp
+++ src/common/communication/ShipPlacementPackage.cpp
@@ -13,12 +13,11 @@ package_nr_t ShipPlacementPackage::get_package_nr() {
 
 std::vector<unsigned char> ShipPlacementPackage::encode_payload() {
     std::vector<unsigned char> encoded = AuthenticatedNetworkPackage::encode_payload();
-    for(auto it = ship_data.begin(); it != ship_data.end(); it++) {
-        auto &ship = *it;
-        encoded.push_back(ship.length & 0xFF);
-        encoded.push_back(ship.orientation & 0x1);
-        encoded.push_back(ship.start_position.y & 0xFF);
-        encoded.push_back(ship.start_position.x & 0xFF);
+    for(int i = 0; i < ship_data.size(); i++) {
+        ShipData &ship = ship_data[i];
+        NetworkPackage::add_to_bytes(encoded,
+            ship.length, ship.orientation,
+            ship.start_position.y, ship.start_position.x);
     }
     return encoded;
 }
@@ -30,14 +29,11 @@ void ShipPlacementPackage::decode_payload(std::vector<unsigned char> command_dat
     const int num_ships = ship_length / ship_length;
     ship_data.clear();
     for(int i = 0; i < num_ships; i++) {
-        const int ship_offset = IDENTITY_LENGTH + i * ship_length;
-        ShipData data = { command_data[ship_offset],
-                          static_cast<orientation_t>(command_data[ship_offset + 1] & 1),
-                          { command_data[ship_offset + 2],
-                            command_data[ship_offset + 3],
-                          },
-                        };
-        ship_data.push_back(data);
+        ShipData ship;
+        NetworkPackage::get_from_bytes(command_data, IDENTITY_LENGTH + i * 4,
+            ship.length, (unsigned char &)ship.orientation,
+            ship.start_position.y, ship.start_position.x);
+        ship_data.push_back(ship);
     }
 }
 
