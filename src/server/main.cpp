af9f05a08b03d21aadb48ac9de539d29f52e6b6e
Moritz Küttel <moritz.kuettel@hslu.ch>
2015-04-13 17:47:21 +0200

    display cbattleship text client in ascii-art when starting the client
    also make sure the output kind of matches with the ascii-art
    output of the cbattleship server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -3,13 +3,13 @@
 
 int main(int argc, char *argv[]) {
     std::cout
-    << "v0.1  __        __  __  __        __   _ " << std::setw(32) << ("built on "  __DATE__) << "\n"
+    << "v0.1  __        __  __  __        __   _ " << std::setw(31) << ("built: "  __DATE__) << "\n"
     << " ____/ /  ___ _/ /_/ /_/ /__ ___ / /  (_)__    ___ ___ _____  _____ ____\n"
     << "/ __/ _ \\/ _ `/ __/ __/ / -_|_-</ _ \\/ / _ \\  (_-</ -_) __/ |/ / -_) __/\n"
     << "\\__/_.__/\\_,_/\\__/\\__/_/\\__/___/_//_/_/ .__/ /___/\\__/_/  |___/\\__/_/   \n"
     << "  by Troopa & Mogria           GPLv3 /_/ github.com/troopa/cBattleship  \n"
     << "This program comes with ABSOLUTELY NO WARRANTY; This is free software,\n"
-    << "and you are welcome to redistribute it under certain conditions."
+    << "and you are welcome to redistribute it under certain conditions.\n"
     << std::endl;
 
     LogConfig logConfig(std::string(argv[0]) + ".log", true);
--- src/text-client/main.cpp
+++ src/text-client/main.cpp
@@ -11,19 +11,32 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    std::cout
+    << "v0.1  __        __  __  __        __   _    \n"
+    << " ____/ /  ___ _/ /_/ /_/ /__ ___ / /  (_)__ \n"
+    << "/ __/ _ \\/ _ `/ __/ __/ / -_|_-</ _ \\/ / _ \\\n"
+    << "\\__/_.__/\\_,_/\\__/\\__/_/\\__/___/_//_/_/ .__/\n"
+    << "GPLv3  github.com/troopa/cBattleship /_/    \n"
+    << "  __          __        ___          __ \n"
+    << " / /______ __/ /_  ____/ (_)__ ___  / /_\n"
+    << "/ __/ -_) \\ / __/ / __/ / / -_) _ \\/ __/\n"
+    << "\\__/\\__/_\\_\\\\__/  \\__/_/_/\\__/_//_/\\__/\n"
+    << "by Troopa & Mogria " << std::setw(20) <<  ("built: " __DATE__) << "\n"
+    << "This program comes with ABSOLUTELY NO WARRANTY; This is free software,\n"
+    << "and you are welcome to redistribute it under certain conditions.\n"
+    << std::endl;
+
     LogConfig logClient(std::string(argv[0]) + ".log");
 
-    std::cout << "resolving ... " << std::endl;
+    std::cout << "trying to connect to '" << argv[1] << "' ... " << std::endl;
     boost::asio::io_service io_service;
     boost::asio::ip::tcp::resolver resolver(io_service);
     boost::asio::ip::tcp::resolver::query query(argv[1], "13477");
     boost::asio::ip::tcp::socket socket(io_service);
     boost::asio::connect(socket, resolver.resolve(query));
-    std::cout << "connecting ... " << std::endl;
+    std::cout << "trying to join server... " << std::endl;
 
     Connection connection(1, std::move(socket));
-    PlayerJoinPackage player_join_package;
-
     try {
         SimpleClient client(connection);
         client.run();
@@ -34,3 +47,4 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
+
