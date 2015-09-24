d9a261d789fd3046a011690109983f9036a42deb
Mogria <m0gr14@gmail.com>
2015-09-24 17:59:13 +0200

    added GameConfiguration to hold configuration for the game, which is shared between server and client.
    GameServerConfiguration contains GameConfiguration and configuration
    specific to the game server like bind-address and port

---
+++ src/common/GameConfiguration.h
@@ -0,0 +1,23 @@
+#ifndef _GAMECONFIGURATION_H
+#define _GAMECONFIGURATION_H
+
+#include <common/packages/NetworkPackage.h>
+#include <boost/program_options/variables_map.hpp>
+
+class GameConfiguration {
+    private:
+        bool hitspree;
+
+    public:
+        GameConfiguration();
+        GameConfiguration(boost::program_options::variables_map &parameters);
+        virtual ~GameConfiguration();
+        
+        bool get_hitspree();
+
+        std::vector<unsigned char> to_bytes();
+        void from_bytes(std::vector<unsigned char> &bytes, unsigned int index);
+};
+
+
+#endif /* _GAMECONFIGURATION_H */
