8255df81e42d363ffe9705a215cdb24aa61867ee
Mogria <m0gr14@gmail.com>
2015-10-05 21:50:52 +0200

    call ClientStateMachine::run() in seperate thread, have a dispatcher

--- src/gtk-client/Client.h
+++ src/gtk-client/Client.h
@@ -1,6 +1,10 @@
 #ifndef _CLIENT_H
 #define _CLIENT_H
 
+#include <glibmm/thread.h>
+#include <glibmm/threads.h>
+#include <glibmm/dispatcher.h>
+#include <gtkmm/textview.h>
 #include "MainWindow.h"
 #include <clientlib/ClientStateMachine.h>
 #include <memory>
@@ -9,9 +13,15 @@ class Client {
     private:
         Glib::RefPtr<Gtk::Builder> builder;
         MainWindow *main_window;
+        Gtk::TextView *log_textview;
 
         std::unique_ptr<ClientStateMachine> state_machine;
 
+        Glib::Dispatcher dispatcher;
+        mutable Glib::Threads::Mutex dispatch_mutex;
+        sigc::connection dispatch_connection;
+
+        Glib::Thread *state_machine_thread;
     private:
         Client();
         virtual ~Client();
@@ -20,6 +30,10 @@ class Client {
         static Client &get();
 
         MainWindow &get_window();
+
+        void dispatch(std::function<void(void)> func);
+
+        void log(std::string text);
 
         void connect(std::string name, std::string ip_address, unsigned short port);
 };
