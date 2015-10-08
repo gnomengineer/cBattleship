5e7db28e9d1ee141d42584e2dd17f6c7af5e3d2b
Mogria <m0gr14@gmail.com>
2015-10-08 21:04:10 +0200

    remade tests for NetworkPackageManager

--- src/test/NetworkPackageManagerTest.cpp
+++ src/test/NetworkPackageManagerTest.cpp
@@ -1,33 +1,46 @@
 #include "NetworkPackageManagerTest.h"
+#include <iostream>
+#include <iomanip>
 
 CPPUNIT_TEST_SUITE_REGISTRATION(NetworkPackageManagerTest);
 
-void NetworkPackageManagerTest::setUp()
-{
+void NetworkPackageManagerTest::setUp() {
     network_package_manager = new NetworkPackageManager();
     // doesn't need to be added everytime, is just overwritten
     network_package_manager->add_network_packages<TestNetworkPackage>();
+
+    TestNetworkPackage test_package;
+    std::string name = "chuck testa";
+    test_package.set_player_name(name);
+    encoded = network_package_manager->encode_package(test_package);
+
+    network_package = network_package_manager->decode_package(encoded);
 }
 
 
-void NetworkPackageManagerTest::tearDown()
-{
+void NetworkPackageManagerTest::tearDown() {
     delete network_package_manager;
 }
 
-void NetworkPackageManagerTest::encode_package_test()
-{
-    TestNetworkPackage test_package;
-    auto vector = network_package_manager->encode_package(test_package);
-    CPPUNIT_ASSERT_EQUAL((int)0x08, (int)vector[1]);
-    CPPUNIT_ASSERT_EQUAL((int)0x00, (int)vector[2]);
-    CPPUNIT_ASSERT_EQUAL((int)0x01, (int)vector[0]);
-    CPPUNIT_ASSERT_EQUAL((int)0xCD, (int)vector[vector.size() - 1]);
+void NetworkPackageManagerTest::encode_package_test() {
+    unsigned int length = encoded.size();
+    CPPUNIT_ASSERT_EQUAL((int)(length & 255), (int)encoded[0]);
+    CPPUNIT_ASSERT_EQUAL((int)((length >> 8) & 255), (int)encoded[1]);
+    CPPUNIT_ASSERT_EQUAL((int)0xCD, (int)encoded[length - 1]);
 }
 
-void NetworkPackageManagerTest::decode_package_test()
-{
-    CPPUNIT_FAIL("not implemented");
+void NetworkPackageManagerTest::decode_package_nr_test() {
+    CPPUNIT_ASSERT_EQUAL(NetworkPackageManager::get_package_nr<TestNetworkPackage>(),
+                         network_package.package_nr());
+}
+
+void NetworkPackageManagerTest::decode_inner_package_test() {
+    bool ret = NetworkPackageManager::handle_package<TestNetworkPackage>(network_package,
+        [](TestNetworkPackage & package) {
+            CPPUNIT_ASSERT_EQUAL(std::string("chuck testa"), package.player_name());
+        }
+    );
+    CPPUNIT_ASSERT(ret);
 }
 
 
--- src/test/NetworkPackageManagerTest.h
+++ src/test/NetworkPackageManagerTest.h
@@ -11,21 +11,27 @@ class NetworkPackageManagerTest : public CppUnit::TestFixture
     CPPUNIT_TEST_SUITE(NetworkPackageManagerTest);
 
     CPPUNIT_TEST(encode_package_test);
-    CPPUNIT_TEST(decode_package_test);
+    CPPUNIT_TEST(decode_package_nr_test);
+    CPPUNIT_TEST(decode_inner_package_test);
 
     CPPUNIT_TEST_SUITE_END();
 
     private:
-        class TestNetworkPackage : public GameReadyPackage {
+        class TestNetworkPackage : public PlayerJoinPackage {
         };
         NetworkPackageManager *network_package_manager;
+
+        std::vector<unsigned char> encoded;
+
+        NetworkPackage network_package;
       
     public:
         void setUp();
         void tearDown();
           
         void encode_package_test();
-        void decode_package_test();
+        void decode_package_nr_test();
+        void decode_inner_package_test();
 
 };
 
